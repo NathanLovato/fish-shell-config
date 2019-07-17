afb35176bb98fa7ab8894c4bd14b133aa97a6894
Nathan Lovato <nathan@gdquest.com>
2019-07-17 11:02:00 +0200

    Improve function to create blender projects
    Create nonexistent project directories
    Shorten some of the code
    Use argparse

--- functions/video_create_blender_projects.fish
+++ functions/video_create_blender_projects.fish
@@ -1,10 +1,12 @@
-function video_create_blender_projects --description ''
-    set ERROR_MISSING_FOLDERS "You need to pass folder(s) to this function for it to work"
+function video_create_blender_projects --description 'Create one or more '
+    argparse --name=video_create_blender_projects --min-args 1 'd/date=' -- $argv
+    or return
 
-    set folders_list (filter_folders $argv)
-    if [ $folders_list ]
-        echo $ERROR_MISSING_FOLDERS
-        return
+    set ERROR_MISSING_FOLDERS "You need to pass folder(s) to this function for it to work"
+    for dir in $argv
+        if not test -d $dir
+            mkdir $dir
+        end
     end
 
     set EXT_VIDEO mp4,mov,mts,mkv,MP4,MOV,MTS,MKV
@@ -12,24 +14,29 @@ function video_create_blender_projects --description ''
     set EXT_AUDIO wav,mp3,aac,flac
 
     set blender_template_file $HOME/Templates/video.blend
-    set blender_python_file $HOME/.config/fish/functions/blender_import_footage.py
+    set blender_python_file ./blender_import_footage.py
 
-    for dir in $folders_list:
-        set new_name $dir/$dir.blend
-        echo "Creating blend file" $new_name
-        cp $blender_template_file $new_name
+    echo "Creating directories and moving files"
+    set start_folder (pwd)
+    for dir in $argv
+        cd $dir
 
-        echo "Creating directories and moving files"
-        mkdir $dir/video $dir/audio $dir/img
-        for file in $dir/*.{(echo $EXT_VIDEO)}
-            mv $dir/$file $dir/video/$file
+        set blend_file_path $dir.blend
+        echo "Creating blend file" $blend_file_path
+        cp $blender_template_file $blend_file_path
+
+        mkdir video audio img
+        for file in *.{(echo $EXT_VIDEO)}
+            mv $file video/$file
         end
-        for file in $dir/*.{(echo $EXT_IMG)}
-            mv $dir/$file $dir/img/$file
+        for file in *.{(echo $EXT_IMG)}
+            mv $file img/$file
         end
-        for file in $dir/*.{(echo $EXT_AUDIO)}
-            mv $dir/$file $dir/audio/$file
+        for file in *.{(echo $EXT_AUDIO)}
+            mv $file audio/$file
         end
-        blender $dir/$dir.blend -b -P $blender_python_file
+
+        # blender $blend_file_path -b -P $blender_python_file
+        cd $start_folder
     end
 end
