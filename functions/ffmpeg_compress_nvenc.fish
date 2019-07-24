45d5608f20489263eb1b6d85f765e8e63b6be834
Nathan Lovato <nathan@gdquest.com>
2019-07-24 10:58:08 +0200

    Add function to re-encode videos as low size h264 with ffmpeg and nvenc

---
+++ functions/ffmpeg_compress_nvenc.fish
@@ -0,0 +1,21 @@
+function ffmpeg_compress_nvenc --description "Compresses video files passed as arguments using h264_nvenc"
+    argparse --name=ffmpeg_compress_nvenc --min-args 1 'd/delete' -- $argv
+    or return
+
+    set extensions (string split " " "mp4 mov mkv")
+    set file_list (filter_files $argv --extensions $extensions)
+    if not [ $file_list ]
+        echo "None of the file paths exists, cancelling"
+        return
+    end
+
+    for f in $file_list
+        set directory (dirname $f)
+        set file (basename $f)
+        set name (string split -m 1 -r '.' $file)[1]
+        ffmpeg -i $f -c:a copy -c:v h264_nvenc -preset slow -qp 20 $directory/$name-compressed.mp4
+        if [ $_flag_delete ]
+            rm $f
+        end
+    end
+end
