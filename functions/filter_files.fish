54c2f3b10d02617b0452ed2ab31989e1a650d1e2
Nathan Lovato <nathan@gdquest.com>
2020-01-10 11:33:13 +0100

    Add option to ignore the case in filter_files

--- functions/filter_files.fish
+++ functions/filter_files.fish
@@ -1,12 +1,16 @@
 function filter_files --description "Outputs a list of existing files, optionally filtered with a list of extensions"
-    argparse --name=filter_files --min-args 1 'e/extensions=' -- $argv
+    argparse --name=filter_files --min-args 1 'e/extensions=' 'i/ignore_case' -- $argv
     or return
 
     set extensions (string split " " $_flag_extensions)
+    if [ $_flag_ignore_case ]
+        set extensions (echo $extensions | tr [:upper:] [:lower:])
+    end
+
     set files
     for f in $argv
         if not test -f $f
-            echo $f does not exist
+            echo "$f does not exist or is not a file"
             continue
         end
 
@@ -14,7 +18,7 @@ function filter_files --description "Outputs a list of existing files, optionall
         set extension (string split "." --right $name)[-1]
 
         if not contains $extension $extensions
-            echo "$f doesn't have a valid extension in $extensions"
+            # echo "$extension isn't a valid extension in $extensions"
             continue
         end
         set files $files $f
