20835a65972057d6d218ff995ddc16c5e6c4ff72
Nathan Lovato <nathan@gdquest.com>
2020-01-12 13:58:43 +0100

    Update EDITOR and VISUAL for editing on the fly from fish
    Make EDITOR open emacsclient in the terminal
    Make VISUAL open a frame
    Make the shell wait for the program to close, e.g. when using Alt E to edit the
    command in fish

--- conf.d/config.fish
+++ conf.d/config.fish
@@ -1,5 +1,5 @@
-set -gx EDITOR emacsclient -cn
-set -gx VISUAL emacsclient -cn -a ""
+set -gx EDITOR emacsclient -t -a ""
+set -gx VISUAL emacsclient -c -a ""
 set -gx ALTERNATE_EDITOR nvim
 
 source $HOME/.config/fish/conf.d/abbreviations.fish
