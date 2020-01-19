c088c3cef682f23e03ca04ff16e335f5f153e72b
Nathan Lovato <nathan@gdquest.com>
2020-01-19 17:55:11 -0600

    Fix open_godot_project not finding the project in upwards directories

--- functions/open_godot_project.fish
+++ functions/open_godot_project.fish
@@ -5,10 +5,14 @@ function open_godot_project --description 'Searches for Godot projects and opens
     or return
 
     if [ $_flag_help ]
-        echo "Searches for Godot projects and opens the first project found."
-        echo "The executable godot must be available on the PATH"
-        echo "Usage: open_godot_project [OPTIONS] -- arguments
+        echo "
+        Searches for Godot projects and opens the first project found.
+        The executable godot must be available on the PATH
 
+        Usage:
+        open_godot_project [OPTIONS] -- arguments
+
+        Flags:
         -h/--help -- Display this help message.
         -p/--path -- Starting path to search for the project.
         -e/--edge -- Use godot-edge as the Godot executable instead of godot. The executable must be available on the PATH.
@@ -23,27 +27,14 @@ function open_godot_project --description 'Searches for Godot projects and opens
 
     set project_file (find $_flag_path -iname project.godot -print -quit)
     if not [ $project_file ]
-        set parent_directories (get_parent_directory -r -m 5)
-        for directory in $parent_directories
-            set found_file (find $directory -maxdepth 1 -iname project.godot -print -quit)
-            if test -f $found_file
-                set project_file $found_file
-                break
-            end
-        end
-    end
-    if not [ $project_file ]
-        echo "No project file found in sub-folders and parent directories."
-        return
+        set project_file ($_flag_path)
     end
 
-    if [ $_flag_run ]
-        set project_file (dirname $project_file)
-    end
+    set flag_editor '-e'
+    if [ $_flag_run ]; set flag_editor ''; end
 
     set executable godot
-    if [ $_flag_edge ]
-        set executable godot-edge
-    end
-    $executable $project_file $argv
+    if [ $_flag_edge ]; set executable godot-edge; end
+
+    $executable $flag_editor -u $project_file $argv
 end
