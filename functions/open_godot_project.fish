4dcb6d4ea1ac142cec33702be5928998c0078a9b
Nathan Lovato <nathan@gdquest.com>
2020-01-29 11:12:30 -0600

    Update open godot project to autocomplete versions of Godot in /usr/lib/bin

--- functions/open_godot_project.fish
+++ functions/open_godot_project.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 function open_godot_project --description 'Searches for Godot projects and opens the first project found.'
-    argparse --name=open_godot_project 'p/path=' 'e/edge' 'r/run' 'h/help' -- $argv
+    argparse --name=open_godot_project 'p/path=' 'e/executable=' 'r/run' 'h/help' -- $argv
     or return
 
     if [ $_flag_help ]
@@ -13,10 +13,11 @@ function open_godot_project --description 'Searches for Godot projects and opens
         open_godot_project [OPTIONS] -- arguments
 
         Flags:
-        -h/--help -- Display this help message.
-        -p/--path -- Starting path to search for the project.
-        -e/--edge -- Use godot-edge as the Godot executable instead of godot. The executable must be available on the PATH.
-        -r/--run  -- Play the game instead of opening the editor.
+        -h/--help     -- Display this help message.
+        -p/--path     -- Starting path to search for the project.
+        -e/--edge     -- Use godot-edge as the Godot executable instead of godot. The executable must be available on the PATH.
+        -e/--executable  -- Use a specific Godot executable, from the PATH, or from /usr/local/bin/.
+        -r/--run      -- Play the game instead of opening the editor.
 
         This program passes all extra arguments as-is to the godot executable.
         For example, this command will run the Godot project full-screen:
@@ -34,7 +35,11 @@ function open_godot_project --description 'Searches for Godot projects and opens
     if [ $_flag_run ]; set flag_editor ''; end
 
     set executable godot
-    if [ $_flag_edge ]; set executable godot-edge; end
+    if [ $_flag_executable ]
+        set executable $_flag_executable
+    end
 
     $executable $flag_editor -u $project_file $argv
 end
+
+complete -f -c open_godot_project -s e -l executable -a (string join " " (find /usr/local/bin/ -iname 'godot?.?'))
