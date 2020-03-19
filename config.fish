97d843cc315d50b1a7e845b100cead968ea440da
Nathan Lovato <nathan@gdquest.com>
2020-03-19 07:58:50 -0600

    Add variables for text formatting

--- config.fish
+++ config.fish
@@ -5,3 +5,24 @@ set -gx ALTERNATE_EDITOR nvim
 source $HOME/.config/fish/conf.d/abbreviations.fish
 alias godot godot3.2
 
+if not set --query ARE_FORMAT_VARS_INITIALIZED
+    echo 'Setting format_* variables for text formatting'
+
+    set -Ux ARE_FORMAT_VARS_INITIALIZED
+
+    set -Ux format_normal (tput sgr0)
+    set -Ux format_bold (tput bold)
+    set -Ux format_italic (tput sitm)
+    set -Ux format_underline (tput smul)
+
+    set -Ux format_color_black (tput setaf 1)
+    set -Ux format_color_red (tput setaf 1)
+    set -Ux format_color_green (tput setaf 2)
+    set -Ux format_color_yellow (tput setaf 3)
+    set -Ux format_color_blue (tput setaf 4)
+    set -Ux format_color_magenta (tput setaf 5)
+    set -Ux format_color_cyan (tput setaf 6)
+    set -Ux format_color_white (tput setaf 7)
+
+    set -Ux format_blink (tput blink)
+end
